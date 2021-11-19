328764da3d4bd7094177949b1ba4562824064fa8
Nodge <matheus.silva3@ftc.edu.br>
2021-11-19 16:45:29 -0300

    fix: round up pet random list (floow)

--- src/commands/pet.ts
+++ src/commands/pet.ts
@@ -4,9 +4,10 @@ import {resolveEmoji} from '../namespaces/utils.js'
 const petPhrases = [
   "**Uxi got some scratches**",
   "T T Thank you...",
-  "Uxi is now happy",
+  "Uxi is happy now",
   "**If Uxi had a tail, it would be wagging**",
-  "uwu"
+  "uwu",
+  "https://cdn.discordapp.com/attachments/763858663583776778/911138076489838602/Uxi_petpet.gif"
 ]
 export default new app.Command({
   name: "pet",
@@ -14,6 +15,6 @@ export default new app.Command({
   channelType: "all",
   async run(message) {
     const emoji =  await resolveEmoji(message.guild, "uxiblush")
-    message.send(`${emoji} ${petPhrases[Math.random() * petPhrases.length]}`)
+    message.send(`${emoji} ${petPhrases[Math.floor(Math.random() * petPhrases.length)]}`)
   }
 })
