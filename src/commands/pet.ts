c2d89946b7a4539443bb83f9a1658759bd6c2996
Nodge <matheus.silva3@ftc.edu.br>
2021-11-19 00:18:13 -0300

    feat: pet comand with random line

--- src/commands/pet.ts
+++ src/commands/pet.ts
@@ -1,12 +1,19 @@
 import * as app from "../app.js"
 import {resolveEmoji} from '../namespaces/utils.js'
 
+const petPhrases = [
+  "**Uxi got some scratches**",
+  "T T Thank you...",
+  "Uxi is now happy",
+  "**If Uxi had a tail, it would be wagging**",
+  "uwu"
+]
 export default new app.Command({
   name: "pet",
-  description: "The pet command",
+  description: "Pet's me uwu",
   channelType: "all",
   async run(message) {
     const emoji =  await resolveEmoji(message.guild, "uxiblush")
-    message.send(`${emoji} T-T-Thank you\n~~Uxi received pet~~`)
+    message.send(`${emoji} ${petPhrases[Math.random() * petPhrases.length]}`)
   }
 })
